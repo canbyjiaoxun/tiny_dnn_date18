d395d7351c70c9706699966cf3ee5bcbd54c41b3
date18 <date18@yaxin.ucsd.edu>
2017-09-03 22:25:19 -0700

    update bloom filter based on freqop

--- tiny_dnn/core/kernels/BV.h
+++ tiny_dnn/core/kernels/BV.h
@@ -7,6 +7,9 @@
 #include <string>
 #include <iostream>
 #include <map>
+#include <tuple>
+#include "freq_op.h"
+
 namespace tiny_dnn {
 namespace kernels {
 
@@ -18,25 +21,34 @@ inline std::string floatToBinary(float x){
          int   output;
     }    data;
     data.input = x;
-    std::bitset<sizeof(float) * 8>   bits(data.output);
+    std::bitset<sizeof(float) * 8> bits(data.output);
     return bits.to_string(); 
 }
 
+
+
 //approximate bloom filter hit 
-inline float check_BV(float op1, float op2) {
-    std::map<std::string, float> freq_pattern;
-    freq_pattern[''] = 0.5; 
-    std:string op1_str = floatToBinary(op1);
-    std:string op2_str = floatToBinary(op2);
-    if (freq_pattern[(op1_str, op2_str)].find() != freq_pattern.end())
-       return freq_pattern[(op1_str, op2_str)];
-       
+inline float check_BV(float op1, float op2, int layer) {
+    std::map<std::tuple<std::string, std::string>, float> freq_pattern;
+    if (layer == 1) 
+       freq_pattern = BF1(); 
+    else if (layer == 2)
+       freq_pattern = BF2(); 
+    else 
+       freq_pattern = BF3(); 
+    std::string op1_str = floatToBinary(op1);
+    std::string op2_str = floatToBinary(op2);
+    //std::cout << "op1: " << op1 << " " << op1_str.substr(0,13) << std::endl; 
+    //std::cout << "op2: " << op2 << " " << op2_str.substr(0,13) << std::endl; 
+    if (freq_pattern.find(std::make_tuple(op1_str.substr(0,13), op2_str.substr(0,13))) != freq_pattern.end())
+    {
+       //std::cout << "Hit " << op1 << op2 << std::endl; 
+       return freq_pattern[std::make_tuple(op1_str.substr(0,13), op2_str.substr(0,13))]; 
+    }  
+    else
+       return (op1 * op2); 
 
 }
-
-
-
-
 
 
 
