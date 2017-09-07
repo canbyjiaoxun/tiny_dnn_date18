431c01690280d32b46a800f6207c524093377e97
date18 <date18@yaxin.ucsd.edu>
2017-09-06 23:14:12 -0700

    can match based on two decimal places

--- operands/extract_freq_op.py
+++ operands/extract_freq_op.py
@@ -6,14 +6,15 @@ from collections import defaultdict
 size_list = [10, 20, 30, 40, 50] 
 
 
-op_file = open('conv2d_MUL_op_layer3_CNN')
+op_file = open('test.txt')
 #op_file = open('conv2d_MUL_op_layer1_CNN')
 op_list = op_file.readlines()
 d =defaultdict(int)
 for op in op_list:
     op_split = op.split()
     if len(op_split) == 2:
-       op_tuple = (op_split[0], op_split[1])
+       #op_tuple = (op_split[0], op_split[1])
+       op_tuple = (str(round(float(op_split[0]),2)), str(round(float(op_split[1]),2)))
        d[op_tuple] += 1
 for size in size_list: 
     print sorted(d.values(), reverse=True)[:size] 
